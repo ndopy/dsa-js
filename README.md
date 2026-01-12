042ea95569e787d1232441d201a84fc8a6834bb5
hugo <vvhooping@gmail.com>
2026-01-12 19:42:57 +0900

    Feat: 연결 리스트 구현

---
+++ README.md
@@ -0,0 +1,35 @@
+<span style="font-size: 2rem; font-weight: bold;">참고하는 강의</span>
+  - [그림으로 쉽게 배우는 자료구조와 알고리즘 (기본편)](https://www.inflearn.com/course/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B8%B0%EB%B3%B8/)
+  - [그림으로 쉽게 배우는 자료구조와 알고리즘 (심화편)](https://www.inflearn.com/course/%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%89%BD%EA%B2%8C-%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EC%8B%AC%ED%99%94)
+
+<br/>
+
+<span style="font-size: 2rem; font-weight: bold;">학습 기간</span>
+- 시작일자 : 2026-01-12 월
+- 종료일자 : 진행중
+
+<br/>
+
+<span style="font-size: 2rem; font-weight: bold;">학습 목표</span>
+- 자료구조와 알고리즘의 기초 원리를 이해합니다.
+- **JavaScript**를 사용하여 직접 코드를 구현합니다.
+
+<br/>
+
+<span style="font-size: 2rem; font-weight: bold;">자료구조</span>
+- [ ] 연결 리스트 (Linked List)
+- [ ] 스택 (Stack)
+- [ ] 큐 (Queue)
+- [ ] 덱 (Deque)
+- [ ] 해시 테이블 (Hash Table)
+- [ ] 셋 (Set)
+
+<span style="font-size: 2rem; font-weight: bold;">알고리즘</span>
+- [ ] 재귀 (Recursion)
+- [ ] 정렬 - 버블 정렬
+- [ ] 정렬 - 선택 정렬
+- [ ] 정렬 - 삽입 정렬
+- [ ] 정렬 - 병합 정렬
+- [ ] 정렬 - 퀵 정렬
+- [ ] 동적 프로그래밍 - 메모이제이션
+- [ ] 동적 프로그래밍 - 타뷸레이션
---
+++ src/01-linked-list/LinkedList.mjs
@@ -0,0 +1,108 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  printAll() {
+    let text = '[';
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      text += currentNode.data;
+      currentNode = currentNode.next;
+
+      if (currentNode !== null) {
+        text += ', ';
+      }
+    }
+
+    text += ']';
+    console.log(text);
+  }
+
+  clear() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  insertAt(index, data) {
+    if (index < 0 || index > this.count) {
+      throw new Error('범위를 벗어났습니다.');
+    }
+
+    const newNode = new Node(data);
+
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
+    }
+
+    this.count += 1;
+  }
+
+  insertLast(data) {
+    this.insertAt(this.count, data);
+  }
+
+  deleteAt(index) {
+    if (index < 0 || index >= this.count) {
+      throw new Error('범위를 벗어났습니다.');
+    }
+
+    if (index === 0) {
+      let deleteNode = this.head;
+      this.head = this.head.next;
+      this.count -= 1;
+
+      return deleteNode;
+    } else {
+      let currentNode = this.head;
+
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+
+      let deleteNode = currentNode.next;
+      currentNode.next = currentNode.next.next;
+      this.count -= 1;
+      return deleteNode;
+    }
+  }
+
+  deleteLast() {
+    return this.deleteAt(this.count - 1);
+  }
+
+  getNodeAt(index) {
+    if (index < 0 || index >= this.count) {
+      throw new Error('범위를 벗어났습니다.');
+    }
+
+    let currentNode = this.head;
+
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
+}
+
+export { Node, LinkedList };
